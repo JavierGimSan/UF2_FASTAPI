123bc5871e5566d7ca6e4f402a093dc3f0f61df9
JavierGimSan <javi.gimsa@gmail.com>
2024-11-13 20:51:40 +0100

    EXERCICI 7, excepció mitjançant HTTP Exception

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, status
+## IMPORTS ##
+from fastapi import FastAPI, status, HTTPException 
 from pydantic import BaseModel
 
-##Declaro el basemodel per fer el POST:
+## Declaro el basemodel per fer el POST:
 class Usuari(BaseModel):
     nom: str
     cognom: str
@@ -12,10 +13,12 @@ class Usuari(BaseModel):
 
 app = FastAPI()
 
+##GET que retorna un missatge de prova.
 @app.get("/")
 async def read_root():
     return {"message": "Prova de mètode GET"}
 
+##GET que retorna un diccionari de 3 camps.
 @app.get("/get2")
 async def read_segonGet():
     return {
@@ -24,6 +27,21 @@ async def read_segonGet():
             "tercer_camp": "últim"
             }
 
+##Aquest POST agafa el BaseModel de dalt i el retorna. Els camps s'introdueixen al postman o al swagger per fer proves.
+##També retorna un error introduit manualment mitjançant 'status_code'
 @app.post("/introduirUsuari", status_code=status.HTTP_404_NOT_FOUND)
 async def enter_user(usuari: Usuari):
     return usuari
+
+##FUNCIÓ DE L'EXERCICI 7
+usuaris = {
+    "nom": str,
+    "edat": int,
+    "aula": str
+}
+
+@app.get("/usuaris/{cognom}")
+async def read_cognom(cognom: str):
+    if cognom not in usuaris:
+        raise HTTPException(status_code=404, detail="No s'ha trobat el camp")
+    return {"usuari": usuaris[cognom]}
